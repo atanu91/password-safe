f0288958ab4dab1b6cb73155a535fe3448806a29
Atanu Chatterjee <chatterjee.atanu1991@gmail.com>
2021-10-12 23:53:36 +0530

    updated api routes

--- src/backend/api/flask/passwordSafeApi.py
+++ src/backend/api/flask/passwordSafeApi.py
@@ -1,22 +1,32 @@
 from flask import Flask, jsonify
 
-password_safe_app = Flask(__name__)
+password_safe_api = Flask(__name__)
 
 
-@password_safe_app.route("/")
+@password_safe_api.route("/api")
 def say_hello():
-    return "Hello World<br>This is the landing page"
+    return jsonify("Hello World! This is the api landing page")
 
 
-@password_safe_app.route("/login", methods=["GET", "POST"])
-def test():
-    return jsonify("test")
+@password_safe_api.route("/api/create")
+def user_create():
+    return jsonify("Hello World! This is the api landing page")
 
 
-@password_safe_app.route("/api")
-def say2_hello():
+@password_safe_api.route("/api/update")
+def user_update():
+    return jsonify("Hello World! This is the api landing page")
+
+
+@password_safe_api.route("/api/{user_hash}/delete")
+def user_delete():
+    return jsonify("Hello World! This is the api landing page")
+
+
+@password_safe_api.route("/api/{user_hash}/reset")
+def user_delete():
     return jsonify("Hello World! This is the api landing page")
 
 
 if __name__ == "__main__":
-    password_safe_app.run(host='0.0.0.0', debug=True)
+    password_safe_api.run(host='0.0.0.0', debug=True)
